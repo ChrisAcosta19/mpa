f28237dc72dff5ab73cb84d6744e0822d4b11870
Christopher Acosta <chriacos@espol.edu.ec>
2023-12-17 18:48:49 -0500

    IMDB

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HttpClientModule } from  '@angular/common/http';
@@ -15,16 +15,19 @@ import { DatospeliculaService } from '../../providers/datospelicula.service'
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
+  movieIds: string[] = ['tt0111161', 'tt0468569', 'tt1375666', 'tt0137523',
+   'tt0109830', 'tt0167260', 'tt0110912', 'tt0120737',
+    'tt0133093', 'tt0245429', 'tt0068646', 'tt0050083'];
   //Atributo con el tipo de dato de la interfaz
   public data : Pelicula[] = [];
   //Inyección de dependencia del servicio
   constructor(private dataProvider: DatospeliculaService) { }
  //Ejecución de la petición y suscripción de la respuesta
-  ngOnInit() {
-   this.dataProvider.getResponse().subscribe((response) => { 
-     let dataArray = (response as Pelicula[]); 
-     this.data = dataArray.slice(0,10);
-   })
-}
+
+  ngOnInit(): void {
+    this.dataProvider.getMoviesByIds(this.movieIds).subscribe((movies) => {
+      this.data = movies;
+    });
+  }
 }
--- src/app/providers/datospelicula.service.ts
+++ src/app/providers/datospelicula.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
 //Importación del HttpClient
 import { HttpClient } from '@angular/common/http';
+import { Observable, forkJoin } from 'rxjs';
+import { Pelicula } from '../interfaz/pelicula';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class DatospeliculaService {
-  //Atributo URL
-  private URL: string = 'https://tiendapeliculasonline-default-rtdb.firebaseio.com/collection.json';
+  private apiUrl = 'http://www.omdbapi.com';
+  private apiKey = '14f06f65'; // Reemplaza con tu clave de API real
+
+
   //Inyección de dependencia del HttpClient
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   //Método con la petición HTTP
-  getResponse() {
-    return this.http.get(this.URL);
-}
+  getMoviesByIds(ids: string[]): Observable<Pelicula[]> {
+    const requests = ids.map((id) =>
+      this.http.get<Pelicula>(`${this.apiUrl}/?apikey=${this.apiKey}&i=${id}`)
+    );
+
+    // Combinar las solicitudes en paralelo
+    return forkJoin(requests);
+  }
 }
